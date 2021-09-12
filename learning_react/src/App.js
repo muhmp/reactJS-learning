b55b8c653ef7b763a53d8e54d36a1c0daaa46275
muhmp <defcoolz@gmail.com>
2021-09-12 15:04:46 +0900

    creating example table using react

---
+++ learning_react/src/App.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react'
+
+//create class
+class App extends Component{
+    render(){
+        return(
+            <div className="App">
+                <h1>Hello, React !</h1>
+            </div>
+        )
+    }
+}
+
+export default App
---
+++ learning_react/src/Table.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react'
+
+//create the class
+class Table extends Component{
+    render(){//render method
+        return( //return with parentheses
+            //elemnt
+            <table>
+                <thead>
+                    
+                </thead>
+            </table>
+
+        )
+    }
+}
